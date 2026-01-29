d54bad8cda2e49f8ae32a66d6a86ea6753c00830
Mikiyas <159655111+Mikiyas199@users.noreply.github.com>
2026-01-29 23:04:52 +0300

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -39,21 +39,3 @@ async def channel_post(client: Client, message: Message):
         except Exception:
             pass
 
-@Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
-async def new_post(client: Client, message: Message):
-
-    if DISABLE_CHANNEL_BUTTON:
-        return
-
-    converted_id = message.id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    try:
-        await message.edit_reply_markup(reply_markup)
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        await message.edit_reply_markup(reply_markup)
-    except Exception:
-        pass
